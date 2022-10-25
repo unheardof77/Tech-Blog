0efc173d82b99eae47996a88d42d1675d82a11cd
Morgan Tolman <morgan.tolman04@gmail.com>
2022-10-24 23:14:58 -0700

    Started on handlebars.  Safe revert point.

--- controllers/htmlRoutes/htmlRoutes.js
+++ controllers/htmlRoutes/htmlRoutes.js
@@ -1,6 +1,13 @@
 const router = require(`express`).Router();
-
-
+const { Post, Comment, User } = require(`../../models`)
+router.get(`/`, async (req,res) =>{
+    try{
+        const data = await Post.findAll({raw: true})
+        res.status(200).render(`home`, {data});
+    } catch (err){
+        res.status(500).json(err);
+    };
+})
 
 
 module.exports = router;
